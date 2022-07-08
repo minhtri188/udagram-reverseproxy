7bc440b4363db40654d59fc21c619d3025f607e4
Tri Tran <minhtri1881@gmail.com>
2022-07-08 15:56:02 +0700

    init project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Admin123
+export POSTGRES_HOST=my-postgre-db.c3moh0spkrv5.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=arn:aws:s3:::my-bucket-p3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=mt
+export JWT_SECRET=mt123456
+export URL=http://localhost:8100
